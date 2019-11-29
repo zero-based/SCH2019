735b186a47ae517faee47fa184cd19b57ec5ef96
MonicaTanios <monicatanios@gmail.com>
2019-11-29 15:23:52 +0200

    [PARC] Add Google Maps

--- parc/ios/Runner/AppDelegate.m
+++ parc/ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyAtiHe2Q5LfHaPe1-ITQtZpAvfFnx6c_x4";
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- parc/lib/screens/home_screen.dart
+++ parc/lib/screens/home_screen.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:parc/models/userLocation.dart';
+import 'package:parc/services/locationService.dart';
+import 'package:provider/provider.dart';
 
 import '../models/user.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   final User _user;
 
   HomeScreen(this._user);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return StreamProvider<UserLocation>(
+      create: (context) => LocationService().locationStream,
+      child: MaterialApp(
+          home: Scaffold(
+        body: MapWidget(),
+      )),
+    );
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class MapWidget extends StatelessWidget {
+  static final MarkerId markerId = MarkerId('Berlin');
+  final Marker marker = Marker(position: LatLng(52.5200, 13.4050), markerId: markerId);
+  final Set<Marker> markers = new Set<Marker>();
+  final TextEditingController _searchLocationController =
+      new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Center(
-        child: Text("Welcome ${widget._user.name} to Parc!"),
+    var x = Provider.of<UserLocation>(context);
+    markers.add(marker);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('HOME'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search ...',
+                ),
+                controller: _searchLocationController,
+              ),
+              Expanded(
+                flex: 1,
+                child: GoogleMap(
+                  markers: markers,
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  zoomGesturesEnabled: true,
+                  initialCameraPosition: CameraPosition(
+                      target: LatLng(x.latitude, x.longitude), zoom: 300),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
