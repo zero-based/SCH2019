979072bd940fa4a5e9d222aad6a96f9ce1e70251
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2019-11-29 11:40:14 +0200

    [ARCADE] Add Parcade Main Logic Using Leds
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- parcade/src/index.ts
+++ parcade/src/index.ts
@@ -3,14 +3,68 @@ import io = require('socket.io-client');
 
 const socket = io(`http://localhost:${process.env.PORT}/`);
 const board = new five.Board();
+let ignoreIr: Boolean = true;
+let parked: Boolean = false;
 
 board.on("ready", function () {
-    const led = new five.Led(7);
-    led.off();
+    const greenLed = new five.Led(2);
+    const redLed = new five.Led(3);
+    const motion = new five.Motion(4);
+    greenLed.on();
+    redLed.off();
+
+    /* Socket Callbacks */
+
+    socket.on("arrival", function (data: any) {
+        console.log(data);
+        greenLed.off();
+        redLed.on();
+        ignoreIr = false;
+        //TODO: Parcade down here.
+    });
 
     socket.on("newReservation", function (data: any) {
         console.log(data);
-        led.toggle();
+        greenLed.off();
+        redLed.on();
+        //TODO: Parcade up here.
+    });
+
+
+    /* IR Sensor Callbacks */
+
+    /*
+     *   "calibrated" occurs once, at the beginning of a session.
+    */
+    motion.on("calibrated", function () {
+        console.log("calibrated", Date.now());
+    });
+
+    /*
+     *   "motionstart" events are fired when the "calibrated" area is disrupted,
+          generally by some form of movement.
+    */
+    motion.on("motionstart", function () {
+        if (parked) {
+            console.log("motionstart", Date.now());
+            // Left spot
+            greenLed.on();
+            redLed.off();
+            ignoreIr = true;
+            parked = false;
+            // motor up
+        }
+    });
+
+    /*
+     *   "motionend" events are fired following a "motionstart" event
+         when no movement has occurred in X ms.
+    */
+    motion.on("motionend", function () {
+        if (!ignoreIr) {
+            parked = true;
+            console.log("motionend", Date.now());
+        }
     });
 
 });
