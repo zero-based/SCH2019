424adae3ee36d264507fbfbc4381b3ce573c631d
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-11-29 21:03:37 +0200

    [PARC] Refactor HomeScreen

--- parc/lib/screens/home_screen.dart
+++ parc/lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:parc/models/userLocation.dart';
-import 'package:parc/services/locationService.dart';
+import 'package:parc/services/location_service.dart';
 import 'package:provider/provider.dart';
 
 import '../models/user.dart';
@@ -13,56 +12,56 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserLocation>(
+    return StreamProvider<LatLng>(
       create: (context) => LocationService().locationStream,
       child: MaterialApp(
-          home: Scaffold(
-        body: MapWidget(),
-      )),
+        title: "Home",
+        home: Scaffold(
+          body: MapWidget(),
+        ),
+      ),
     );
   }
 }
 
 class MapWidget extends StatelessWidget {
-  static final MarkerId markerId = MarkerId('Berlin');
-  final Marker marker = Marker(position: LatLng(52.5200, 13.4050), markerId: markerId);
-  final Set<Marker> markers = new Set<Marker>();
-  final TextEditingController _searchLocationController =
-      new TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
+  final Set<Marker> markers = Set<Marker>()
+    ..add(Marker(
+      markerId: MarkerId('Berlin'),
+      position: LatLng(52.5200, 13.4050),
+    ));
 
   @override
   Widget build(BuildContext context) {
-    var x = Provider.of<UserLocation>(context);
-    markers.add(marker);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('HOME'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search ...',
-                ),
-                controller: _searchLocationController,
-              ),
-              Expanded(
-                flex: 1,
-                child: GoogleMap(
-                  markers: markers,
-                  myLocationEnabled: true,
-                  myLocationButtonEnabled: true,
-                  zoomGesturesEnabled: true,
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(x.latitude, x.longitude), zoom: 300),
-                ),
-              ),
-            ],
+    var currentLocation = Provider.of<LatLng>(context);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search ...',
+            ),
+            controller: _searchController,
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: GoogleMap(
+              markers: markers,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              zoomGesturesEnabled: true,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  currentLocation.latitude,
+                  currentLocation.longitude,
+                ),
+                zoom: 300,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
