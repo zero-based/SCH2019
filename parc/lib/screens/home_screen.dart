91b3a1bbe75ba3273b48ebbbe1adb1aefe0b8bcf
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-11-30 03:16:28 +0200

    [PARC] Restore App Bar in HomeScreen

--- parc/lib/screens/home_screen.dart
+++ parc/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/user.dart';
+import 'account_screen.dart';
 import 'map_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,6 +12,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Parc'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AccountScreen(_user),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: MapScreen(),
     );
   }
